0acef999c6430711151ab2e5888a649e5d5fdcfc
GwenCodes <essexdesigns43.0@gmail.com>
2025-10-09 22:48:30 -0400

    Created footer with links to Github and Netlify

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This weather app was created by{" "}
+        <a href="https://github.com/GwenCodes" target="_blank">
+          Gwen
+        </a>{" "}
+        open sourced in <a href="https://github.com/GwenCodes">Github</a> and{" "}
+        hosted in{" "}
+        <a href="https://app.netlify.com/teams/gwencodes/projects">Netlify</a>
+      </footer>
     </div>
   );
 }
