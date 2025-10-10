1ff3655f512b9e0f0db0fc9db7554977aa6bca17
GwenCodes <essexdesigns43.0@gmail.com>
2025-10-09 23:17:59 -0400

    Installed bootstrap and added footer with links to Github and Netlify

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This weather app was created by{" "}
-        <a href="https://github.com/GwenCodes" target="_blank">
-          Gwen
-        </a>{" "}
-        open sourced in <a href="https://github.com/GwenCodes">Github</a> and{" "}
-        hosted in{" "}
-        <a href="https://app.netlify.com/teams/gwencodes/projects">Netlify</a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This weather app was created by{" "}
+          <a
+            href="https://github.com/GwenCodes"
+            target="_blank"
+            rel="noreferrer">
+            Gwen
+          </a>{" "}
+          open sourced in <a href="https://github.com/GwenCodes">Github</a> and{" "}
+          hosted in{" "}
+          <a href="https://app.netlify.com/teams/gwencodes/projects">Netlify</a>
+        </footer>
+      </div>
     </div>
   );
 }
