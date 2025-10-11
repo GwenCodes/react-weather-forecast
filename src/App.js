75209a7102da6de3059b0ef7481c69803b1ddc7b
GwenCodes <essexdesigns43.0@gmail.com>
2025-10-11 18:38:18 -0400

    Moved footer out of container

--- src/App.js
+++ src/App.js
@@ -7,19 +7,16 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="Paris" />
-        <footer>
-          This weather app was created by{" "}
-          <a
-            href="https://github.com/GwenCodes"
-            target="_blank"
-            rel="noreferrer">
-            Gwen
-          </a>{" "}
-          open sourced in <a href="https://github.com/GwenCodes">Github</a> and{" "}
-          hosted in{" "}
-          <a href="https://app.netlify.com/teams/gwencodes/projects">Netlify</a>
-        </footer>
       </div>
+      <footer>
+        This weather app was created by{" "}
+        <a href="https://github.com/GwenCodes" target="_blank" rel="noreferrer">
+          Gwen
+        </a>{" "}
+        open sourced in <a href="https://github.com/GwenCodes">Github</a> and{" "}
+        hosted in{" "}
+        <a href="https://app.netlify.com/teams/gwencodes/projects">Netlify</a>
+      </footer>
     </div>
   );
 }
