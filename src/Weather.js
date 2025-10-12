586e4fcac892cfa6a24f00e85caff6408c4b85ac
GwenCodes <essexdesigns43.0@gmail.com>
2025-10-12 14:25:41 -0400

    added api integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,55 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              autoFocus="on"
-              autoComplete="off"
-              className="form-control"
-            />
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      date: "Wednesday, 0700",
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      iconUrl: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      city: response.data.city,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                autoFocus="on"
+                autoComplete="off"
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul className="weather-details">
+          <li>{weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
+        <div className="row" mt-1>
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src={weatherData.iconUrl}
+                alt={weatherData.description}
+                className="float-left"
+                mt-3
+              />
+              <span className="temperature">{weatherData.temperature}</span>
+              <span className="units">°C</span>
+            </div>
           </div>
-        </div>
-      </form>
-      <h1>Paris</h1>
-      <ul className="weather-details">
-        <li>Day and time</li>
-        <li>Weather conditions</li>
-      </ul>
-      <div className="row" mt-1>
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://www.gstatic.com/weather/conditions/v1/svg/clear_night_light.svg"
-              alt="clear evening"
-              className="float-left"
-              mt-3
-            />
-            <span className="temperature">65</span>
-            <span className="units">°F</span>
+          <div className="col-6">
+            <ul className="weather-details">
+              <li>{weatherData.humidity}%</li>
+              <li>{weatherData.wind} km/h</li>
+            </ul>
           </div>
-        </div>
-        <div className="col-6">
-          <ul className="weather-details">
-            <li>Precipitation: 0%</li>
-            <li>Humidity: 62%</li>
-            <li>Wind: 5mph</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "61dfa07e8o8462teba3fadad30e70d77";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultcity}&key=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
