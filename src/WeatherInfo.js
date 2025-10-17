8cb55ef1adec81771df4182fcf25eae2af29d4fc
GwenCodes <essexdesigns43.0@gmail.com>
2025-10-17 10:08:29 -0400

    Updated WeatherInfo.js codemapping

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -8,13 +8,13 @@ export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
-      <ul className="weather-details">
+      <ul>
         <li>
           <FormattedDate date={props.data.date} />
         </li>
-        <li>{props.data.description}</li>
+        <li className="text-capitalize">{props.data.description}</li>
       </ul>
-      <div className="row nt-1">
+      <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
             <div className="float-left">
@@ -25,12 +25,13 @@ export default function WeatherInfo(props) {
             </div>
           </div>
         </div>
-      </div>
-      <div className="col-6">
-        <ul className="weather-details">
-          <li>Humidity: {props.data.humidity}%</li>
-          <li>Wind: {props.data.wind} km/h</li>
-        </ul>
+
+        <div className="col-6">
+          <ul className="extra-stats">
+            <li> Humidity: {props.data.humidity}%</li>
+            <li>Wind: {Math.round(props.data.wind)} km/hr</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
